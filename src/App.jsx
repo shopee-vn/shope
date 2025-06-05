acee90b8d0de337bddf1b2a796c9a175a39b93c2
shopee <mailwep.1.ok@gmail.com>
2025-06-05 22:55:30 +0700

    update -v4

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ProductDetail } from "./Pages/ProductDetail";
 import { Home } from "./Pages/Home";
 import { CheckOut } from "./Pages/CheckOut";
+import { ScrollToTop } from "./Component/ScrollToTop";
 
 function App() {
     return (
         <>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/product/:id" element={<ProductDetail />} />
