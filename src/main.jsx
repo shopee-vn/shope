a50efcdb606fea58977fd14cebca442dfdaaa6d6
shopee <mailwep.1.ok@gmail.com>
2025-06-06 06:22:20 +0700

    update v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, HashRouter } from "react-router";
 import "./index.css";
-import 'antd/dist/reset.css';
+import "antd/dist/reset.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 );
